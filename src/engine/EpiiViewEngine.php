cc64fdcc077672dfa8da1044fbeb9031871e3d91
MrRen <543169072@qq.com>
2019-02-14 14:54:57 +0800

    init

--- src/engine/EpiiViewEngine.php
+++ src/engine/EpiiViewEngine.php
@@ -47,6 +47,52 @@ class EpiiViewEngine implements IEpiiViewEngine
             $this->config["tpl_end"] = "\\}";
         }
 
+        self::addFunction("options",function($list,$select_value=null){
+
+
+            if (  is_array($list))
+            {
+                $out = "";
+                foreach ($list as $key=>$value)
+                {
+                    if (!is_array($value))
+                    {
+                        $out.="<option value='".$key."'>".$value."</option>";
+                    }else{
+                        $_v = $key;
+                        if (isset($value["id"]))
+                        {
+                            $_v = $value["id"];
+                        }
+                        if (isset($value["value"]))
+                        {
+                            $_v = $value["value"];
+                        }
+                        $_name = "";
+                        if (isset($value["name"]))
+                        {
+                            $_name = $value["name"];
+                        }
+                        if (isset($value["text"]))
+                        {
+                            $_name = $value["text"];
+                        }
+                        $select = "";
+                        // var_dump($args);
+                        if (  ($select_value!==null && $_v==$select_value)  || (isset($value["selected"]) && $value["selected"]))
+                        {
+                            $select = "selected";
+                        }
+                        $out.="<option value='".$_v."' ".$select.">".$_name."</option>";
+
+                    }
+                }
+            }
+
+            return $out;
+        });
+
+
 
     }
 
